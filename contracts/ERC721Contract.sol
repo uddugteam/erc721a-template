56f400f3a5af7545524a5b19a21737a1f51ae11b
asolovov <andre.solovov@gmail.com>
2023-03-13 15:14:40 +0300

    contract

---
+++ contracts/ERC721Contract.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "erc721a/contracts/ERC721A.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ERC721Contract is ERC721A, Ownable {
+
+    constructor(string memory name_, string memory symbol_)
+    ERC721A(name_, symbol_)
+    {}
+
+}
