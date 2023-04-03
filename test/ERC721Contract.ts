8cef20e0fe797066429e4285c0cb19061e148f41
Stellarthoughts <49097167+Stellarthoughts@users.noreply.github.com>
2023-04-03 16:49:08 +0700

    fix: rename warnings and typos

--- test/ERC721Contract.ts
+++ test/ERC721Contract.ts
@@ -2,46 +2,49 @@ import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
+// RENAME!
 describe("ERC721Contract unit tests", function () {
-  const Name = "Name";
-  const Symbol = "Symbol";
+  const Name = "Name"; // RENAME!
+  const Symbol = "Symbol"; // RENAME!
 
-  async function deployMetazokuFixture() {
+  async function deployFixture() {
     const [owner, address1, address2, address3] = await ethers.getSigners();
 
-    const ERC721Contract = await ethers.getContractFactory("ERC721Contract");
-    const contract = await ERC721Contract.deploy(Name, Symbol);
+    const contractFileName = __filename.split("/").splice(-1)[0].split(".").splice(0)[0];
+
+    const Contract = await ethers.getContractFactory(contractFileName); // RENAME!
+    const contract = await Contract.deploy(Name, Symbol); // RENAME!
 
     return { contract, owner, address1, address2, address3 };
   }
 
   describe("Deployment", function () {
     it("Should set the right owner", async function () {
-      const { contract, owner } = await loadFixture(deployMetazokuFixture);
+      const { contract, owner } = await loadFixture(deployFixture);
 
       expect(await contract.owner()).to.equal(owner.address);
     });
 
     it("Should deploy with proper address", async function () {
-      const { contract } = await loadFixture(deployMetazokuFixture);
+      const { contract } = await loadFixture(deployFixture);
 
       expect(contract.address).to.be.properAddress;
     });
 
     it("Should have right name", async function () {
-      const { contract } = await loadFixture(deployMetazokuFixture);
+      const { contract } = await loadFixture(deployFixture);
 
       expect(await contract.name()).to.equal(Name);
     });
 
     it("Should have right symbol", async function () {
-      const { contract } = await loadFixture(deployMetazokuFixture);
+      const { contract } = await loadFixture(deployFixture);
 
       expect(await contract.symbol()).to.equal(Symbol);
     });
 
     it("Should have 0 total supply", async function () {
-      const { contract } = await loadFixture(deployMetazokuFixture);
+      const { contract } = await loadFixture(deployFixture);
       expect(await contract.totalSupply()).to.equal(0);
     });
   });
